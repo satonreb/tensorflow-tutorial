aab47364302abf1359e75b1a1d5da3378a31810f
Tomas <tomas.bernotas@riseup.net>
2017-08-09 21:55:41 +0100

    uoadated two feature script

--- 08_multiRNNCell_LSTM_2_features.py
+++ 08_multiRNNCell_LSTM_2_features.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+import pandas as pd
 from sklearn.model_selection import TimeSeriesSplit
 
 
@@ -43,61 +44,63 @@ def prep_data(array, input_seq_len, output_seq_len, output_seq_steps_ahead, expa
 tf.reset_default_graph()
 # ======================================================================================================================
 # Hyperparameters
-INPUT_SEQUENCE_LENGTH = 28
+INPUT_SEQUENCE_LENGTH = 7
 OUTPUT_SEQUENCE_LENGTH = 1
-OUTPUT_SEQUENCE_STEPS_AHEAD = 14
-N_SPLITS = 4
+OUTPUT_SEQUENCE_STEPS_AHEAD = 4
+N_SPLITS = 2
 BATCH_SIZE = 70
-N_ITERATIONS = 10000
+N_ITERATIONS = 30000
 LSTM_1_N = 16
 INITIAL_LEARNING_RATE = 1e-3
-LEARNING_RATE_DECAY_STEPS = 1000
+LEARNING_RATE_DECAY_STEPS = 5000
 LEARNING_RATE_DECAY_RATE = 0.96
-FC_1_N = 10
-L2_REG_BETA = 0.03
+FC_1_N = 20
 log_file = "graphs/lstm"
 # ======================================================================================================================
-# # Real Wold Data
-# DATA_PATH = "data/euro-foreign-exchange-reference-.csv"
-# df = pd.read_csv(DATA_PATH)
-# df = df[:-3]
-# df['Date'] = df['Date'].map(lambda st: pd.datetime.strptime(st, '%Y-%m-%d'))
-# # **********************************************************************************************************************
-# # These bits have changed
-# today = np.datetime64('today').astype('datetime64[ns]').astype('uint64')
-# X = np.array(df['Date']).astype('uint64') / today
-# # **********************************************************************************************************************
-# Y = np.array(df['Euro foreign exchange reference rates'])
-
+# Real Wold Data
+DATA_PATH = "data/data.csv"
+df = pd.read_csv(DATA_PATH)
+df['Date'] = df['Date'].map(lambda st: pd.datetime.strptime(st, '%Y-%m-%d'))
+df['GBP/EUR'] = pd.to_numeric(df['GBP/EUR'], errors='coerce')
+df.dropna(inplace=True)
+# These bits have changed
+today = np.datetime64('today').astype('datetime64[ns]').astype('uint64')
+X = np.array(df['Date']).astype('uint64') / today
+Y1 = df['GBP/EUR'].values
 # ======================================================================================================================
 # Synthetic Data
-X = np.linspace(start=-5 * np.pi, stop=10 * np.pi, num=500)
-Y = np.sin(X) / 2 - np.sin(-X * 5) + X
-X = X / (10 * np.pi)
-# plt.plot(X, Y)
+# X = np.linspace(start=-5 * np.pi, stop=10 * np.pi, num=500)
+# Y1 = np.sin(X) / 2 - np.sin(-X * 2) + X / 10
+Y2 = np.sin(X*100)
+# X = X / (10 * np.pi)
+# plt.plot(X, Y2)
 # ======================================================================================================================
-
 x_input, x_output = prep_data(array=X, input_seq_len=INPUT_SEQUENCE_LENGTH, output_seq_len=OUTPUT_SEQUENCE_LENGTH,
-                              output_seq_steps_ahead=OUTPUT_SEQUENCE_STEPS_AHEAD, expand_dim=True, expand_dim_axis=2)
-y_input, y_output = prep_data(array=Y, input_seq_len=INPUT_SEQUENCE_LENGTH, output_seq_len=OUTPUT_SEQUENCE_LENGTH,
-                              output_seq_steps_ahead=OUTPUT_SEQUENCE_STEPS_AHEAD, expand_dim=True, expand_dim_axis=2)
-
+                              output_seq_steps_ahead=OUTPUT_SEQUENCE_STEPS_AHEAD, expand_dim=False, expand_dim_axis=2)
+y1_input, y1_output = prep_data(array=Y1, input_seq_len=INPUT_SEQUENCE_LENGTH, output_seq_len=OUTPUT_SEQUENCE_LENGTH,
+                                output_seq_steps_ahead=OUTPUT_SEQUENCE_STEPS_AHEAD, expand_dim=True, expand_dim_axis=2)
+y2_input, y2_output = prep_data(array=Y2, input_seq_len=INPUT_SEQUENCE_LENGTH, output_seq_len=OUTPUT_SEQUENCE_LENGTH,
+                                output_seq_steps_ahead=OUTPUT_SEQUENCE_STEPS_AHEAD, expand_dim=True, expand_dim_axis=2)
 # **********************************************************************************************************************
 # These bits have changed
-input = np.concatenate((x_input, y_input), axis=2)
+input = np.concatenate((y1_input, y2_input), axis=2)
+output = np.concatenate((y1_output, y2_output), axis=2)
 # **********************************************************************************************************************
 
 # Split data into Training and Test datasets
-for train_val_index, test_index in TimeSeriesSplit(n_splits=N_SPLITS).split(y_input):
-    y_input_train_val, y_input_test = input[train_val_index], input[test_index]
-    x_output_train_val, x_output_test = x_output[train_val_index], x_output[test_index]
-    y_output_train_val, y_output_test = y_output[train_val_index], y_output[test_index]
+for train_val_index, test_index in TimeSeriesSplit(n_splits=N_SPLITS).split(input):
+    y_input_test_val, y_input_train = input[train_val_index], input[test_index]
+    x_input_test_val, x_input_train = x_input[train_val_index], x_input[test_index]
+    y_output_test_val, y_output_train = output[train_val_index], output[test_index]
+    x_output_test_val, x_output_train = x_output[train_val_index], x_output[test_index]
+
 
 # Split Test data into Train and Validation datasets
-for train_index, val_index in TimeSeriesSplit(n_splits=N_SPLITS).split(y_input_train_val):
-    y_input_train, y_input_val = y_input_train_val[train_index], y_input_train_val[val_index]
-    x_output_train, x_output_val = x_output_train_val[train_index], x_output_train_val[val_index]
-    y_output_train, y_output_val = y_output_train_val[train_index], y_output_train_val[val_index]
+for train_index, val_index in TimeSeriesSplit(n_splits=N_SPLITS).split(x_input_test_val):
+    y_input_test, y_input_val = y_input_test_val[train_index], y_input_test_val[val_index]
+    x_input_test, x_input_val = x_input_test_val[train_index], x_input_test_val[val_index]
+    y_output_test, y_output_val = y_output_test_val[train_index], y_output_test_val[val_index]
+    x_output_test, x_output_val = x_output_test_val[train_index], x_output_test_val[val_index]
 
 # ======================================================================================================================
 # Define model
@@ -120,33 +123,26 @@ with tf.name_scope('RNN'):
     cells = tf.nn.rnn_cell.MultiRNNCell(cells=[lstm_1, lstm_2])
     rnn_output, _ = tf.nn.dynamic_rnn(cell=cells, inputs=x, dtype=tf.float32)
 
-# Regularisation
-with tf.name_scope('L2_REG'):
-    l2_regulariser = tf.contrib.layers.l2_regularizer(scale=L2_REG_BETA)
-    he_init = tf.contrib.layers.variance_scaling_initializer()  # He initialization
-
 # Define Dense bit
 with tf.name_scope('DNN'):
     output = tf.transpose(a=rnn_output, perm=[1, 0, 2])
     last_rnn = output[-1]
     # last = tf.gather(output, int(output.get_shape()[0]) - 1)
-    fc_1 = tf.layers.dense(inputs=last_rnn, units=FC_1_N, activation=tf.nn.relu,
-                           kernel_initializer=he_init, kernel_regularizer=l2_regulariser)
+    fc_1 = tf.layers.dense(inputs=last_rnn, units=FC_1_N, activation=tf.nn.relu)
     variable_summaries(fc_1)
 
 with tf.name_scope('LR'):
-    y_pred = tf.layers.dense(inputs=fc_1, units=OUTPUT_SEQUENCE_LENGTH)
-    y_pred = tf.expand_dims(input=y_pred, axis=2, name='PREDICTION')
+    y_pred = tf.layers.dense(inputs=fc_1, units=FEATURES)
+    prediction = tf.expand_dims(input=y_pred, axis=1, name='PREDICTION')
     variable_summaries(y_pred)
 
 # ======================================================================================================================
 # Define loss
 with tf.name_scope('LOSS'):
-    y_true = tf.placeholder(dtype=tf.float32, shape=[None, OUTPUT_SEQUENCE_LENGTH, 1], name='TRUTH')
+    y_true = tf.placeholder(dtype=tf.float32, shape=[None, 1, FEATURES], name='TRUTH')
     # y_true = tf.placeholder(dtype=tf.float32, shape=[None, None], name='truth')
-    reconstruction_loss = tf.reduce_mean(input_tensor=tf.square(x=tf.subtract(x=y_pred, y=y_true)))
-    reg_losses = tf.get_collection(key=tf.GraphKeys.REGULARIZATION_LOSSES)
-    loss = tf.add_n([reconstruction_loss] + reg_losses)
+    mse = tf.losses.mean_squared_error(labels=y_true, predictions=prediction)
+    loss = tf.sqrt(x=mse, name='rmse')
     train_step = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss=loss, global_step=global_step)
     variable_summaries(loss)
 
@@ -154,7 +150,6 @@ with tf.name_scope('LOSS'):
 # Train model
 sess = tf.InteractiveSession()
 sess.run(fetches=tf.global_variables_initializer())
-
 
 merged = tf.summary.merge_all()
 writer = tf.summary.FileWriter(log_file, sess.graph)
@@ -180,44 +175,82 @@ for s in range(N_ITERATIONS):
         writer.add_summary(summary, s)
 
 # ======================================================================================================================
-y_predictions_train = y_pred.eval(feed_dict={x: y_input_train})
-y_predictions_val = y_pred.eval(feed_dict={x: y_input_val})
+y_predictions_train = prediction.eval(feed_dict={x: y_input_train})
+y_predictions_val = prediction.eval(feed_dict={x: y_input_val})
 # ======================================================================================================================
 marker_size = 3
 plt.figure()
-plt.plot(y_input_train[:, :, 0], y_input_train[:, :, 1],
+plt.plot(x_input_train, y_input_train[:, :, 0],
          color='black',
          label='train-input')
 
-plt.plot(x_output_train.flatten(), y_predictions_train.flatten(),
+plt.plot(x_input_train, y_predictions_train[:, :, 0],
          color='orange',
          label='train-prediction')
 
-plt.plot(y_input_val[:, :, 0], y_input_val[:, :, 1],
+plt.plot(x_output_val, y_input_val[:, :, 0],
          color='blue',
          label='val-input')
 
-plt.plot(x_output_val.flatten(), y_predictions_val.flatten(),
+plt.plot(x_output_val, y_predictions_val[:, :, 0],
          color='red',
          label='val-prediction')
 
 plt.xlabel("x")
-plt.ylabel("y")
+plt.ylabel("y_1")
+plt.grid()
+plt.legend()
+# ======================================================================================================================
+marker_size = 3
+plt.figure()
+plt.plot(x_input_train, y_input_train[:, :, 1],
+         color='black',
+         label='train-input')
+
+plt.plot(x_input_train, y_predictions_train[:, :, 1],
+         color='orange',
+         label='train-prediction')
+
+plt.plot(x_output_val, y_input_val[:, :, 1],
+         color='blue',
+         label='val-input')
+
+plt.plot(x_output_val, y_predictions_val[:, :, 1],
+         color='red',
+         label='val-prediction')
+
+plt.xlabel("x")
+plt.ylabel("y_0")
 plt.grid()
 plt.legend()
 # ======================================================================================================================
 # Test
 test_loss = loss.eval(feed_dict={x: y_input_test, y_true: y_output_test})
 print("Test loss is: {loss}".format(loss=test_loss))
-y_predictions_test = y_pred.eval(feed_dict={x: y_input_test})
+y_predictions_test = prediction.eval(feed_dict={x: y_input_test})
+# ======================================================================================================================
 plt.figure()
-plt.plot(y_input_test[:, :, 0], y_input_test[:, :, 1],
+plt.plot(x_input_test, y_input_test[:, :, 0],
          color='black',
-         label='train-input')
+         label='test-input')
 
-plt.plot(x_output_test.flatten(), y_predictions_test.flatten(),
+plt.plot(x_output_test, y_predictions_test[:, :, 0],
          color='orange',
-         label='train-prediction')
+         label='test-prediction')
+plt.xlabel("x")
+plt.ylabel("y")
+plt.grid()
+plt.legend()
+plt.show()
+# ======================================================================================================================
+plt.figure()
+plt.plot(x_input_test, y_input_test[:, :, 1],
+         color='black',
+         label='test-input')
+
+plt.plot(x_output_test, y_predictions_test[:, :, 1],
+         color='orange',
+         label='test-prediction')
 plt.xlabel("x")
 plt.ylabel("y")
 plt.grid()
